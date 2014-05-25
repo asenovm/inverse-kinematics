c28f4d4b41bcd4781d876d9cdaf87da2f3f2cbdd
asenovm <asenov.m@gmail.com>
2014-05-25 23:47:11 +0300

    * Initial commit

---
+++ src/edu/fmi/inverse/kinematics/InverseKinematics.java
@@ -0,0 +1,14 @@
+package edu.fmi.inverse.kinematics;
+
+import javax.swing.SwingUtilities;
+
+public class InverseKinematics {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				// blank as for now, but will add content later
+			}
+		});
+	}
+}
