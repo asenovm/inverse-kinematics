50457d29f0908c93aa766210a6ec1a240b445dc0
asenovm <asenov.m@gmail.com>
2014-05-26 00:13:14 +0300

    * Displaying the simulation view as a grid

--- src/edu/fmi/inverse/kinematics/InverseKinematics.java
+++ src/edu/fmi/inverse/kinematics/InverseKinematics.java
@@ -5,12 +5,13 @@ import javax.swing.SwingUtilities;
 import edu.fmi.inverse.kinematics.view.SimulationView;
 
 public class InverseKinematics {
+
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				final SimulationView view = new SimulationView();
-				view.show();
 			}
 		});
 	}
--- src/edu/fmi/inverse/kinematics/view/SimulationView.java
+++ src/edu/fmi/inverse/kinematics/view/SimulationView.java
@@ -1,12 +1,21 @@
 package edu.fmi.inverse.kinematics.view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GraphicsConfiguration;
+import java.awt.GridLayout;
 import java.awt.HeadlessException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SimulationView extends JFrame {
+
+	/**
+	 * {@value}
+	 */
+	private static final long serialVersionUID = -3388697450030097056L;
 
 	/**
 	 * {@value}
@@ -21,12 +30,32 @@ public class SimulationView extends JFrame {
 	/**
 	 * {@value}
 	 */
-	private static final int WIDTH_FRAME = 800;
+	private static final int WIDTH_FRAME = 600;
 
 	/**
 	 * {@value}
 	 */
-	private static final long serialVersionUID = -3388697450030097056L;
+	private static final int NUM_CELLS = 5;
+
+	/**
+	 * {@value}
+	 */
+	private static final int NUM_ROWS = 5;
+
+	/**
+	 * {@value}
+	 */
+	private static final int WIDTH_CELL = WIDTH_FRAME / NUM_CELLS;
+
+	/**
+	 * {@value}
+	 */
+	private static final int HEIGHT_CELL = HEIGHT_FRAME / NUM_ROWS;
+
+	/**
+	 * {@value}
+	 */
+	private static final Color COLOR_BORDER = Color.getHSBColor(0, 0, 26.7f);
 
 	public SimulationView(String title, GraphicsConfiguration gc) {
 		super(title, gc);
@@ -41,6 +70,20 @@ public class SimulationView extends JFrame {
 
 		pack();
 		setVisible(true);
+
+		final GridLayout layout = new GridLayout(NUM_CELLS, NUM_ROWS);
+		setLayout(layout);
+
+		for (int i = 0; i < NUM_CELLS * NUM_ROWS; ++i) {
+			final JPanel cell = new JPanel();
+			cell.setBorder(BorderFactory.createLineBorder(COLOR_BORDER));
+			final Dimension cellDimension = new Dimension(WIDTH_CELL,
+					HEIGHT_CELL);
+			cell.setPreferredSize(cellDimension);
+			cell.setMinimumSize(cellDimension);
+			cell.setMaximumSize(cellDimension);
+			add(cell);
+		}
 	}
 
 	public SimulationView() throws HeadlessException {
